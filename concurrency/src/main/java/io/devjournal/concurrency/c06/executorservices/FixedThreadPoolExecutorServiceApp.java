ac0fff47c95b8f981c2904b99281054c2a97c347
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-09-03 15:18:04 +0530

    Concurrency - ThreadPoolExecutor with Fixed Pool

---
+++ concurrency/src/main/java/io/devjournal/concurrency/c06/executorservices/FixedThreadPoolExecutorServiceApp.java
@@ -0,0 +1,80 @@
+package io.devjournal.concurrency.c06.executorservices;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class FixedThreadPoolExecutorServiceApp {
+
+    static final int MIN_THREAD_POOL_SIZE = 1;
+
+    static final int MAX_THREAD_POOL_SIZE = 3;
+
+    static final long THREAD_KEEP_ALIVE_DURATION = 1_000;
+
+    static final int WAIT_QUEUE_SIZE = 5;
+
+    // F--
+    // Creating the ThreadPoolExecutor Manually to demo purposes. Otherwise, in most cases
+    // you should be using Executors.newFixedThreadPool(MAX_THREAD_POOL_SIZE)
+    static final ExecutorService THREAD_POOL = new ThreadPoolExecutor(
+                                                    MIN_THREAD_POOL_SIZE,
+                                                    MAX_THREAD_POOL_SIZE,
+                                                    THREAD_KEEP_ALIVE_DURATION,
+                                                    TimeUnit.MILLISECONDS,
+                                                    new ArrayBlockingQueue<Runnable>(WAIT_QUEUE_SIZE),
+                                                    new RejectedExecutionHandler() {
+
+                                                        @Override
+                                                        public void rejectedExecution(final Runnable task, final ThreadPoolExecutor executor) {
+                                                            System.out.println("Task submission Rejected - " + task);
+                                                        }
+                                                    }
+                                               );
+    // F++
+
+    public static void main(final String[] args) {
+        // F--
+        IntStream.rangeClosed(1, 10)
+                 .sequential()
+                 .forEach(iteration -> {
+
+                     final String taskName = "task-" + iteration;
+
+                     try {
+                         Thread.sleep(10);
+                         THREAD_POOL.submit(new GCDRunnable(taskName));
+                     } catch (final InterruptedException e) {
+                         System.out.println("Sleep interrupted for Task - " + taskName);
+                     }
+                 });
+        // F++
+
+        try {
+
+            THREAD_POOL.shutdown();
+            System.out.println("THREAD_POOL.shutdown requested. Awaiting 5 seconds for termination");
+
+            THREAD_POOL.awaitTermination(10, TimeUnit.SECONDS);
+            System.out.println("THREAD_POOL.awaitTermination completed");
+
+        } catch (final InterruptedException e) {
+            System.out.println("THREAD_POOL.awaitTermination interrupted");
+        } finally {
+            final boolean terminated = THREAD_POOL.isTerminated();
+            System.out.println("THREAD_POOL.isTerminated - " + THREAD_POOL.isTerminated());
+
+            if (!terminated) {
+                THREAD_POOL.shutdownNow();
+                System.out.println("THREAD_POOL.shutdownNow() completed");
+            }
+
+            System.out.println("THREAD_POOL termination completed");
+        }
+
+        System.out.println("Exiting main()");
+    }
+}
---
+++ concurrency/src/main/java/io/devjournal/concurrency/c06/executorservices/GCDRunnable.java
@@ -0,0 +1,77 @@
+package io.devjournal.concurrency.c06.executorservices;
+
+import java.util.Random;
+
+public class GCDRunnable extends Random implements Runnable {
+
+    private static final long serialVersionUID = 935019882899583928L;
+
+    static final String CLASS_NAME = GCDRunnable.class.getSimpleName();
+
+    static final int NUMBER_LIMIT = 1_000_000;
+
+    static final int TOTAL_ITERATIONS = 25_000_000;
+
+    static final int OUTPUT_STEP_COUNT = 5_000_000;
+
+    static final String LOG_TEMPLATE = "[%s][%s] GCD of Number1[%d] and Number[%d] => %d";
+
+    private final String taskName;
+
+    public GCDRunnable(final String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+
+        final String threadName = getThreadName();
+        final String className = CLASS_NAME + "-" + taskName;
+
+        int number = 1;
+
+        for (; number <= TOTAL_ITERATIONS; number++) {
+
+            final int number1 = nextInt(NUMBER_LIMIT);
+            final int number2 = nextInt(NUMBER_LIMIT);
+
+            final int gcd = gcd(number1, number2);
+
+            if (number % OUTPUT_STEP_COUNT == 0) {
+                System.out.println(String.format(LOG_TEMPLATE, className, threadName, number1, number2, gcd));
+            }
+        }
+
+        System.out.println(String.format("[%s][%s] Exiting thread.run()", className, threadName));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(taskName = %s)", CLASS_NAME, taskName);
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    private int gcd(final int number1, final int number2) {
+
+        final int gcd;
+
+        if (number2 == 0) {
+            gcd = number1;
+        } else {
+            gcd = gcd(number2, number1 % number2);
+        }
+
+        return gcd;
+    }
+
+    private String getThreadName() {
+
+        final boolean daemon = Thread.currentThread().isDaemon();
+        final String threadNamePrefix = daemon ? "Daemon" : "User";
+
+        return threadNamePrefix + Thread.currentThread().getName();
+    }
+}
