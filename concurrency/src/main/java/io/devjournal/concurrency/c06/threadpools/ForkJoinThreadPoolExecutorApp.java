382995c561d32cafdf869f0706678309fa1d8d04
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-09-03 17:12:26 +0530

    Concurrency - Using RecursiveTask with ForkJoinPool

--- concurrency/src/main/java/io/devjournal/concurrency/c06/threadpools/ForkJoinThreadPoolExecutorApp.java
+++ concurrency/src/main/java/io/devjournal/concurrency/c06/threadpools/ForkJoinThreadPoolExecutorApp.java
@@ -3,18 +3,29 @@ package io.devjournal.concurrency.c06.threadpools;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 
 public class ForkJoinThreadPoolExecutorApp {
 
     static final int POOL_SIZE = 2;
 
+    static final List<Integer> NUMBERS = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
     public static void main(final String[] args) {
+        runDouble();
+        System.out.println("====================================");
+        runSum();
+    }
 
+    static void runDouble() {
         final ForkJoinPool pool = new ForkJoinPool(POOL_SIZE);
+        pool.invoke(new DoubleNumberRecursiveAction(NUMBERS));
+    }
 
-        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-        pool.invoke(new DoubleNumberRecursiveAction(numbers));
+    static void runSum() {
+        final ForkJoinPool pool = new ForkJoinPool(POOL_SIZE);
+        final Integer sum = pool.invoke(new SumNumberRecursiveTask(NUMBERS));
+        System.out.println(String.format("Sum of %s -> %s", NUMBERS.toString(), sum));
     }
 }
 
@@ -53,6 +64,53 @@ class DoubleNumberRecursiveAction extends RecursiveAction {
         final DoubleNumberRecursiveAction leftSplit = new DoubleNumberRecursiveAction(numbers.subList(0, mid));
         final DoubleNumberRecursiveAction rightSplit = new DoubleNumberRecursiveAction(numbers.subList(mid, size));
 
-        invokeAll(Arrays.asList(leftSplit, rightSplit));
+        invokeAll(List.of(leftSplit, rightSplit));
+    }
+}
+
+class SumNumberRecursiveTask extends RecursiveTask<Integer> {
+
+    private static final long serialVersionUID = -3908927959129267031L;
+
+    static final int FORK_THRESHOLD = 2;
+
+    private final List<Integer> numbers;
+
+    public SumNumberRecursiveTask(final List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    protected Integer compute() {
+
+        final Integer sum;
+
+        if (numbers.size() <= FORK_THRESHOLD) {
+            sum = doSum();
+        } else {
+            sum = split();
+        }
+
+        return sum;
+    }
+
+    private Integer doSum() {
+        return numbers.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private Integer split() {
+
+        final int size = numbers.size();
+
+        final int mid = size / 2;
+
+        final SumNumberRecursiveTask leftSplit = new SumNumberRecursiveTask(numbers.subList(0, mid));
+        final SumNumberRecursiveTask rightSplit = new SumNumberRecursiveTask(numbers.subList(mid, size));
+
+        // We only fork the rightSplit, because we would like to re-use our current
+        // thread to process leftSplit
+        rightSplit.fork();
+
+        return leftSplit.compute() + rightSplit.join();
     }
 }
